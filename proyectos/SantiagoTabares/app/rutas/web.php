1375850fee80d65ee14f3ec5969d98735b287aa1
SantiagoTabares <satabaresmo@unal.edu.co>
2018-12-01 12:05:48 -0500

    Cuarta sesion

---
+++ proyectos/SantiagoTabares/app/rutas/web.php
@@ -0,0 +1,7 @@
+<?php
+
+// Definición de rutas de la Aplicación Web
+
+$app->get('/', ['App\Controladores\InicioControlador', 'paginaInicio'])->setName('inicio');
+
+$app->get('/aplicacion', ['App\Controladores\AplicacionControlador', 'paginaAplicacion'])->setName('aplicacion');
